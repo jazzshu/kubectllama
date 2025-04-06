d7d1c17dd9b78f8265688668e9be64c27c435d99
JasonShuyinta <jasonshuyinta@gmail.com>
2025-04-06 13:23:20 +0200

    fix

--- cmd/main.go
+++ cmd/main.go
@@ -18,7 +18,7 @@ import (
 type OllamaRequest struct {
 	Model  string `json:"model"`
 	Prompt string `json:"prompt"`
-	Stream bool   `json:"stream"` // Added to control streaming
+	Stream bool   `json:"stream"`
 	System string `json:"system"`
 }
 
